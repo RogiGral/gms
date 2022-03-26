cee816a2d0861176a07f8b4b57836dc1fd82fd6e
DIR\igor.gralak <igor.gralak>
2022-03-26 14:03:54 +0100

    - reInitialize frontend angular

---
+++ frontend/gymApp/src/app/model/user_model.ts
@@ -0,0 +1,19 @@
+export class User {
+  public userId: string;
+  public firstName: string;
+  public lastName: string;
+  public username: string;
+  public email: string;
+  public lastLoginDate: Date;
+  public lastLoginDateDisplay: Date;
+  public joinDate: Date;
+  public profileImageUrl: string;
+  public active: boolean;
+  public notLocked: boolean;
+  public role: string;
+  public authorities: [];
+
+
+
+}
+
--- frontend/gymApp/src/app/service/user.service.ts
+++ frontend/gymApp/src/app/service/user.service.ts
@@ -3,12 +3,13 @@ import {environment} from "../../environments/environment";
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {User} from "../model/user_model";
+import {CustomHttpRespone} from "../model/custom-http-response_model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private host = environment.apuUrl
+  private host = environment.apiUrl
 
   constructor(private http: HttpClient) { }
 
@@ -21,14 +22,14 @@ export class UserService {
   public updateUser(formdata: FormData): Observable<User | HttpErrorResponse>{
     return  this.http.post<User>(`${this.host}/user/update`,formdata);
   }
-  public resetPassword(email: string): Observable<any | HttpErrorResponse>{
-    return this.http.get<any>(`${this.host}/user/resetpassword/${email}`)
+  public resetPassword(email: string): Observable<CustomHttpRespone | HttpErrorResponse>{
+    return this.http.get<CustomHttpRespone>(`${this.host}/user/resetpassword/${email}`)
   }
   public setPassword(email: string, password: string): Observable<any | HttpErrorResponse>{
     return this.http.get<any>(`${this.host}/user/setpassword/${email}/${password}`)
   }
-  public deleteUser(userId: number): Observable<any | HttpErrorResponse>{
-    return this.http.delete<any>(`${this.host}/user/resetpassword/${userId}`)
+  public deleteUser(userId: number): Observable<CustomHttpRespone | HttpErrorResponse>{
+    return this.http.delete<CustomHttpRespone>(`${this.host}/user/resetpassword/${userId}`)
   }
   public addUsersToLocalCache(users: User[]): void{
     localStorage.setItem('users',JSON.stringify(users));
@@ -39,5 +40,20 @@ export class UserService {
     }
     return null;
   }
+
+  public createUserFormDate(loggedInUsername: string, user: User, profileImage: File): FormData {
+      const formData = new FormData();
+      formData.append('currentUsername', loggedInUsername);
+      formData.append('firstName', user.firstName);
+      formData.append('lastName', user.lastName);
+      formData.append('username', user.username);
+      formData.append('email', user.email);
+      formData.append('role', user.role);
+      formData.append('profileImage', profileImage);
+      formData.append('isActive', JSON.stringify(user.active));
+      formData.append('isNonLocked', JSON.stringify(user.notLocked));
+      return formData;
+   }
+
 
 }
